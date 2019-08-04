b4c8badc8d472b67abf43f351acabaef028d28b0
valc0d3r <perellovalentinpro@gmail.com>
2019-08-04 22:52:27 +0200

    Integration page contact.

--- app/src/Views/frontend/contact.php
+++ app/src/Views/frontend/contact.php
@@ -5,15 +5,18 @@
     require_once "./src/Views/banner.php";
 ?>
 
-<section class="contact">
-    <form action="" method="">
-        <input type="text" name="lastname" placeholder="Nom">
-        <input type="text" name="firstname" placeholder="Prenom">
-        <input type="text" name="society" placeholder="Société">
-        <input type="email" name="email" placeholder="Email">
-        <input type="textarea" name="message" placeholder="Message">
-        <button type="submit" class="btn-yellow">Envoyer</button>
-    </form>
-</section>
+<div class="container">
+    <section class="contact">
+        <form class="contact-form" action="" method="">
+            <input type="text" name="lastname" placeholder="NOM" required>
+            <input type="text" name="firstname" placeholder="PRENOM" required>
+            <input type="text" name="society" placeholder="SOCIETE">
+            <input type="email" name="email" placeholder="EMAIL" required>
+            <textarea rows="10" cols="33" name="message" placeholder="MESSAGE">
+            </textarea>
+            <button type="submit" class="btn-yellow">ENVOYER</button>
+        </form>
+    </section>
+</div>
 
 <?php require_once "./src/Views/footer.php"; 
